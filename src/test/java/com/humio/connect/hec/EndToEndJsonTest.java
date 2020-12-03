af84a0144551aa50d4ae02eab266db1e7612a57b
Mike Pedersen <mike@mikepedersen.dk>
2020-12-03 11:25:13 +0100

    Use grapql to get ingest token instead of looking in global-data-snapshot.json
    Finding the ingest token in files doesn't work on Linux due to Dcoker typically running as a root service.

--- src/test/java/com/humio/connect/hec/EndToEndJsonTest.java
+++ src/test/java/com/humio/connect/hec/EndToEndJsonTest.java
@@ -8,7 +8,6 @@ package com.humio.connect.hec;
 
 import com.esotericsoftware.yamlbeans.YamlException;
 import com.esotericsoftware.yamlbeans.YamlReader;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -287,24 +286,35 @@ public class EndToEndJsonTest {
     }
 
     private static String getIngestToken() throws IOException {
-        System.out.println("extracting " + HUMIO_INDEX + " ingest key from global data snapshot...");
-        String s = new String(Files.readAllBytes(Paths.get("humio-data/global-data-snapshot.json")));
-        JsonObject data = new JsonParser().parse(s).getAsJsonObject();
-        JsonObject dataspaces = data.getAsJsonObject("dataspaces");
-        Set<Map.Entry<String, JsonElement>> entries = dataspaces.entrySet();
-        for(Map.Entry<String, JsonElement> entry : entries) {
-            String key = entry.getKey();
-            JsonObject obj = entry.getValue().getAsJsonObject();
-            if (key.startsWith(HUMIO_INDEX) && key.indexOf("LocalHostRoot") == -1) {
-                System.out.println("located " + HUMIO_INDEX + " key: " + key);
-                JsonObject sandbox = dataspaces.getAsJsonObject(key);
-                JsonObject entity = sandbox.getAsJsonObject("entity");
-                JsonArray tokens = entity.getAsJsonArray("ingestTokens");
-                JsonObject token = tokens.get(0).getAsJsonObject();
-                return token.get("token").getAsString();
+        System.out.println("getting " + HUMIO_INDEX + " ingest token via graphql...");
+
+        RequestBody body = RequestBody.create(
+                MediaType.parse("application/json"),
+                "{ \"query\": \"query { repositories { name ingestTokens { token } } }\"} "
+        );
+
+        Request request = new Request.Builder()
+                .url("http://localhost:"+HUMIO_PORT+"/graphql")
+                .post(body)
+                .build();
+
+        Response response = new OkHttpClient().newCall(request).execute();
+        String responseBody = Objects.requireNonNull(response.body()).string();
+        JsonObject data = new JsonParser().parse(responseBody).getAsJsonObject();
+
+        for (JsonElement element : data.getAsJsonObject("data").getAsJsonArray("repositories")) {
+            JsonObject obj = element.getAsJsonObject();
+            if (obj.get("name").getAsString().startsWith(HUMIO_INDEX)) {
+                return obj
+                        .getAsJsonArray("ingestTokens")
+                        .get(0)
+                        .getAsJsonObject()
+                        .get("token")
+                        .getAsString();
             }
         }
-        throw new RuntimeException("cannot extract " + HUMIO_INDEX + " key from humio-data/global-data-snapshot.json");
+
+        throw new RuntimeException("cannot extract " + HUMIO_INDEX + " ingest token via graphql");
     }
 
     private static void registerHumioHECSinkConnector(String configuration) throws IOException {
